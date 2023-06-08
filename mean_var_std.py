dcdde76a5f8568a852ecf924e5666ba65b5fb83e
itokiana-mahefarison <81515527+itokiana-mahefarison@users.noreply.github.com>
2023-06-08 11:47:42 +0000

    python numpy mean, variance, standard deviation, max, min

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,7 +1,24 @@
 import numpy as np
 
 def calculate(list):
+    calculations = {}
+    np_list = np.array(list)
 
+    if(np_list.size < 9):
+      raise ValueError("List must contain nine numbers.")
+    list_reshape = np.reshape(np_list, (3, 3))
+
+    calculations["sum"] = [np.sum(list_reshape, axis = 0), np.sum(list_reshape, axis=1), np.sum(list_reshape)]
+  
+    calculations["min"] = [np.min(list_reshape, axis = 0), np.min(list_reshape, axis=1), np.min(list_reshape)]
+
+    calculations["max"] = [np.max(list_reshape, axis = 0), np.max(list_reshape, axis=1), np.max(list_reshape)]
+
+    calculations["mean"] = [np.mean(list_reshape, axis = 0), np.mean(list_reshape, axis=1), np.mean(list_reshape)]
+
+    calculations["variance"] = [np.var(list_reshape, axis = 0), np.var(list_reshape, axis=1), np.var(list_reshape)]
+
+    calculations["standard deviation"] = [np.std(list_reshape, axis = 0), np.std(list_reshape, axis=1), np.std(list_reshape)]
 
 
 
